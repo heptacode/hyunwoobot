a85cdc503174b58bf2bb88cf341345dc974ac7ea
heptacode <admin@hyunwoo.kim>
2020-12-24 22:48:10 +0900

    Update pm2 Ecosystem Config

--- src/modules/firestore.ts
+++ src/modules/firestore.ts
@@ -1,6 +1,6 @@
 import admin from "firebase-admin";
 
-const serviceAccount = require("../firebase/hyunwoo-bot-5b9111af24ff.json");
+const serviceAccount = require("../../firebase/hyunwoo-bot-5b9111af24ff.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
