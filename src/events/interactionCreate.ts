fee7b14a0950557170624ef48a078505132819f8
Hyunwoo Kim <admin@hyunwoo.kim>
2021-01-21 18:18:37 +0900

    Update interactionCreate.ts

--- src/events/interactionCreate.ts
+++ src/events/interactionCreate.ts
@@ -8,7 +8,7 @@ export default () => {
       const command = commands.get(interaction.data.name) || commands_manager.find((cmd) => cmd.name === interaction.data.name && !cmd.messageOnly);
       if (!command) return;
 
-      command.execute(state.get(client.guilds.cache.get(interaction.guild_id).id), interaction);
+      command.execute(state.get(interaction.guild_id), interaction);
     } catch (err) {
       Log.e(`InteractionCreate > ${err}`);
     }
